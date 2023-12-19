bf3acb98118452bacf867ef3cc99bb35a562da3e
OhBeomho <tyler0322@naver.com>
2023-12-19 19:22:12 +0900

    server info command implemented

--- commands/guilds/serverinfo.ts
+++ commands/guilds/serverinfo.ts
@@ -1,9 +1,30 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, CategoryChannel } from "discord.js";
 import { Command } from "..";
 
 export const command: Command = {
     data: new SlashCommandBuilder().setName("server-info").setDescription("서버 정보"),
     async execute(interaction) {
-        // TODO: get server informations like date created, member count, etc.
+        const guild = interaction.guild;
+        if (!guild) return;
+
+        const guildOwner = await guild.fetchOwner();
+        const embed = new EmbedBuilder()
+            .setTitle(guild.name)
+            .setAuthor({ name: guildOwner.user.tag, iconURL: guildOwner.user.avatarURL() || undefined })
+            .setDescription(guild.description)
+            .addFields(
+                { name: "Created at", value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>` },
+                { name: "Member count", value: String(guild.memberCount) },
+                { name: "Role count", value: String((await guild.roles.fetch()).size - 1), inline: true },
+                {
+                    name: "Channel count",
+                    value: String(
+                        (await guild.channels.fetch()).filter((channel) => !(channel instanceof CategoryChannel)).size
+                    ),
+                    inline: true
+                }
+            )
+            .setThumbnail(guild.iconURL());
+        await interaction.reply({ embeds: [embed] });
     }
 };
