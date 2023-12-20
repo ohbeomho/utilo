fe4cd6b04f9b27159dc1e786d67ed7d42afb04d6
OhBeomho <tyler0322@naver.com>
2023-12-20 16:07:00 +0900

    test commands

--- commands/roles/role/index.ts
+++ commands/roles/role/index.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "discord.js";
-import { Command } from "..";
+import { Command } from "../..";
 
 export const command: Command = {
     data: new SlashCommandBuilder().setName("role").setDescription("역할 관련"),
--- index.ts
+++ index.ts
@@ -1,28 +1,38 @@
 import "dotenv/config";
 import { Client, Collection, Events, GatewayIntentBits, REST, Routes } from "discord.js";
 import { Command } from "./commands";
+import commandInfo from "./commands/commandInfo.json";
 import fs from "fs/promises";
 
-const BOT_TOKEN = process.env.BOT_TOKEN,
-    APP_ID = process.env.APP_ID;
+const requiredVars = ["BOT_TOKEN", "APP_ID", "NODE_ENV"];
+const env = requiredVars.map((name) => ({ name, value: process.env[name] }));
+const undef = env.filter((e) => e.value === undefined);
 
-if (!BOT_TOKEN || !APP_ID) {
-    console.error("BOT_TOKEN, APP_ID가 .env 파일에 정의되어 있어야 합니다.");
+if (undef.length) {
+    console.log(`${undef.map((e) => e.name).join(", ")} is not defined in .env file`);
     process.exit(1);
 }
+
+const [BOT_TOKEN, APP_ID, NODE_ENV] = env.map((e) => e.value!);
 
 async function loadCommands(): Promise<Command[]> {
     return (
         await Promise.all(
             (await fs.readdir("./commands", { recursive: true, withFileTypes: true }))
-                .filter(
-                    (dirent) => dirent.isFile() && !dirent.name.startsWith("index") && !dirent.name.endsWith(".json")
-                )
+                .filter((dirent) => dirent.isFile())
                 .map((commandFile) => import(`./${commandFile.path}/${commandFile.name}`))
         )
     )
         .filter((imported) => imported.command)
-        .map((imported) => imported.command);
+        .map((imported) => {
+            const command: Command = imported.command;
+
+            if (NODE_ENV === "dev") {
+                command.data.setName(command.data.name + "-test");
+            }
+
+            return command;
+        });
 }
 
 const commands = new Collection<string, Command>();
@@ -36,13 +46,13 @@ const client = new Client({
     ]
 });
 
-client.once(Events.ClientReady, () => console.log(`${client.user?.tag}으로 로그인됨`));
+client.once(Events.ClientReady, () => console.log(`Logged in with ${client.user?.tag}`));
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
     const command = commands.get(interaction.commandName);
     if (!command) {
-        console.error(`'${interaction.commandName}'은 존재하지 않는 명령입니다.`);
+        console.error(`Command '${interaction.commandName}' does not exists.`);
         return;
     }
 
@@ -50,7 +60,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
         await command.execute(interaction);
     } catch (err) {
         console.error(err);
-        const message = { content: "명령을 실행하던 중 오류가 발생하였습니다.", ephemeral: true };
+        const message = { content: "An error occurred while executing command.", ephemeral: true };
 
         if (interaction.replied || interaction.deferred) {
             await interaction.followUp(message);
@@ -62,20 +72,38 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
 (async () => {
     const commandArr = await loadCommands();
-    console.log("Loaded commands:");
-    let commandString = "";
-    commandArr.forEach((command) => {
-        commands.set(command.data.name, command);
-        commandString += command.data.name + " ";
-    });
-    console.log(commandString.trim().replaceAll(" ", ", "));
+    commandArr.forEach((command) => commands.set(command.data.name, command));
+
+    console.log(
+        commandInfo.categories
+            .map(
+                (category) =>
+                    category.name +
+                    " commands:\n" +
+                    category.commands
+                        .map((command) => command.name + (command.developing ? " (developing)" : ""))
+                        .join(", ")
+            )
+            .join("\n\n") + "\n"
+    );
 
     const rest = new REST().setToken(BOT_TOKEN);
+    const body = commandArr.map((command) => command.data.toJSON());
 
     try {
-        await rest.put(Routes.applicationCommands(APP_ID), {
-            body: commandArr.map((command) => command.data.toJSON())
-        });
+        if (NODE_ENV === "prod") {
+            await rest.put(Routes.applicationCommands(APP_ID), { body });
+        } else if (NODE_ENV === "dev") {
+            const GUILD_ID = process.env.GUILD_ID;
+            if (!GUILD_ID) {
+                console.log("GUILD_ID should be defined in .env file if NODE_ENV is 'dev'");
+                process.exit(1);
+            }
+
+            await rest.put(Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body });
+        }
+
+        console.log("Registered commands");
     } catch (err) {
         console.error(err);
         process.exit(1);
