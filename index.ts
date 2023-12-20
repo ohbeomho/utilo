fcc24ed2117c31aef5f232df3cdba2a0c80b1e0d
OhBeomho <tyler0322@naver.com>
2023-12-20 17:35:28 +0900

    e

--- index.ts
+++ index.ts
@@ -49,6 +49,7 @@ const client = new Client({
 client.once(Events.ClientReady, () => console.log(`Logged in with ${client.user?.tag}`));
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
+    else if (NODE_ENV === "dev" && interaction.commandName.endsWith("-test")) return;
 
     const command = commands.get(interaction.commandName);
     if (!command) {
