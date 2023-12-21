94346715f48515367b829635ab9355c216e128c5
OhBeomho <tyler0322@naver.com>
2023-12-21 20:58:36 +0900

    more checking

--- commands/members/ban.ts
+++ commands/members/ban.ts
@@ -12,20 +12,24 @@ export const command: Command = {
 
         const err = (content: string) => interaction.reply({ content, ephemeral: true });
 
-        const user = interaction.member as GuildMember;
-        if (!user.permissions.has(PermissionFlagsBits.BanMembers)) {
+        const member = interaction.member as GuildMember;
+        if (!member.permissions.has(PermissionFlagsBits.BanMembers)) {
             await err("당신에게 사용자를 차단(Ban)할 권한이 없습니다.");
             return;
         }
 
         const target = interaction.options.getMember("user")! as GuildMember;
-        if (user.roles.highest.comparePositionTo(target.roles.highest) < 0) {
+        if (member.roles.highest.comparePositionTo(target.roles.highest) < 0) {
             await err(`${target.user.tag}님이 당신보다 권한이 높아 차단할 수 없습니다.`);
-            return;
-        } else if (!target.kickable) {
+        } else if (!target.bannable) {
             await err(`저에게 ${target.user.tag}님을 차단할 권한이 없습니다.`);
-            return;
+        } else if (target.user.id === member.user.id) {
+            await err("자기 자신을 차단할 수 없습니다.");
+        } else if (target.user.id === interaction.client.user.id) {
+            await err("저는 저를 차단시킬 수 없습니다.");
         }
+
+        if (interaction.replied) return;
 
         await target.ban({ reason: interaction.options.getString("reason") || undefined });
         await interaction.reply(`${target.user.tag}님이 차단되었습니다.`);
--- commands/members/kick.ts
+++ commands/members/kick.ts
@@ -12,20 +12,24 @@ export const command: Command = {
 
         const err = (content: string) => interaction.reply({ content, ephemeral: true });
 
-        const user = interaction.member as GuildMember;
-        if (!user.permissions.has(PermissionFlagsBits.KickMembers)) {
+        const member = interaction.member as GuildMember;
+        if (!member.permissions.has(PermissionFlagsBits.KickMembers)) {
             await err("당신에게 사용자를 추방할 권한이 없습니다.");
             return;
         }
 
         const target = interaction.options.getMember("user")! as GuildMember;
-        if (user.roles.highest.comparePositionTo(target.roles.highest) < 0) {
+        if (member.roles.highest.comparePositionTo(target.roles.highest) < 0) {
             await err(`${target.user.tag}님이 당신보다 권한이 높아 추방할 수 없습니다.`);
-            return;
         } else if (!target.kickable) {
             await err(`저에게 ${target.user.tag}님을 추방할 권한이 없습니다.`);
-            return;
+        } else if (target.user.id === member.user.id) {
+            await err("자기 자신을 차단할 수 없습니다.");
+        } else if (target.user.id === interaction.client.user.id) {
+            await err("저는 저를 차단시킬 수 없습니다.");
         }
+
+        if (interaction.replied) return;
 
         await target.kick(interaction.options.getString("reason") || undefined);
         await interaction.reply(`${target.user.tag}님이 추방되었습니다.`);
