1cb8cb0fbad656baa219c6433a11ffe94ccf556e
OhBeomho <tyler0322@naver.com>
2023-12-21 20:15:23 +0900

    korean log messages, command checking

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const env = requiredVars.map((name) => ({ name, value: process.env[name] }));
 const undef = env.filter((e) => e.value === undefined);
 
 if (undef.length) {
-    console.log(`${undef.map((e) => e.name).join(", ")} is not defined in .env file`);
+    console.log(`${undef.map((e) => e.name).join(", ")}이 .env에 정의되어 있지 않습니다.`);
     process.exit(1);
 }
 
@@ -46,14 +46,18 @@ const client = new Client({
     ]
 });
 
-client.once(Events.ClientReady, () => console.log(`Logged in with ${client.user?.tag}`));
+client.once(Events.ClientReady, () => console.log(`${client.user?.tag}으로 로그인됨`));
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    else if (NODE_ENV === "prod" && interaction.commandName.endsWith("-test")) return;
+    if (
+        !interaction.isChatInputCommand() ||
+        (NODE_ENV === "prod" && interaction.commandName.endsWith("-test")) ||
+        (NODE_ENV === "dev" && !interaction.commandName.endsWith("-test"))
+    )
+        return;
 
     const command = commands.get(interaction.commandName);
     if (!command) {
-        console.error(`Command '${interaction.commandName}' does not exists.`);
+        console.error(`'${interaction.commandName}' - 존재하지 않는 명령입니다.`);
         return;
     }
 
@@ -61,7 +65,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
         await command.execute(interaction);
     } catch (err) {
         console.error(err);
-        const message = { content: "An error occurred while executing command.", ephemeral: true };
+        const message = { content: "명령을 실행하던 중 오류가 발생하였습니다.", ephemeral: true };
 
         if (interaction.replied || interaction.deferred) {
             await interaction.followUp(message);
@@ -94,17 +98,17 @@ client.on(Events.InteractionCreate, async (interaction) => {
     try {
         if (NODE_ENV === "prod") {
             await rest.put(Routes.applicationCommands(APP_ID), { body });
+            console.log("명령어 등록됨");
         } else if (NODE_ENV === "dev") {
             const GUILD_ID = process.env.GUILD_ID;
             if (!GUILD_ID) {
-                console.log("GUILD_ID should be defined in .env file if NODE_ENV is 'dev'");
+                console.error("NODE_ENV가 'dev'이므로 GUILD_ID가 .env에 정의되어 있어야 합니다.");
                 process.exit(1);
             }
 
             await rest.put(Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body });
+            console.log("테스트용 명령어 등록됨");
         }
-
-        console.log("Registered commands");
     } catch (err) {
         console.error(err);
         process.exit(1);
