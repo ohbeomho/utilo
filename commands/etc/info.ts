79f6053d899b1e0eb167249f90846e99fb5789eb
OhBeomho <tyler0322@naver.com>
2023-12-22 17:29:35 +0900

    new command

--- commands/etc/info.ts
+++ commands/etc/info.ts
@@ -1,10 +1,17 @@
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { Command } from "..";
+import { VER } from "../../config";
 
 export const command: Command = {
-    // data: new SlashCommandBuilder().setName("help").setDescription("명령어 목록 표시"),
-    data: "info",
+    data: new SlashCommandBuilder().setName("info").setDescription("봇 정보 표시"),
     async execute(interaction) {
-        await interaction.reply({ content: "개발중", ephemeral: true });
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("UTILO")
+                    .setDescription("간단한 디스코드 봇\n[소스코드](https://github.com/OhBeomho/utilo)")
+                    .addFields({ name: "버전", value: VER }, { name: "개발자", value: "ohbeomho" })
+            ]
+        });
     }
 };
